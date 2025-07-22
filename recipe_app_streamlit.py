8eec2a9ffd7a7131bbf461f21477f47e1f36d431
mackaveli07 <60677021+mackaveli07@users.noreply.github.com>
2025-07-22 09:50:50 -0500

    Update recipe_app_streamlit.py

--- recipe_app_streamlit.py
+++ recipe_app_streamlit.py
@@ -43,7 +43,7 @@ def load_recipes_sql():
         recipes.append({
             "id": row[0],
             "name": row[1],
-            "ingredients": row[2].split("\n"),
+            "ingredients": row[].split("\n"),
             "instructions": row[3],
             "serving_size": row[4],
             "nutrition": {
