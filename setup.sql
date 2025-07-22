7c60fae3e19b60661f947fffb51f695cbfad1c41
mackaveli07 <60677021+mackaveli07@users.noreply.github.com>
2025-07-22 13:13:20 -0500

    Update setup.sql

--- setup.sql
+++ setup.sql
@@ -1,10 +1,10 @@
+DROP TABLE IF EXISTS Recipes;
 
 CREATE TABLE Recipes (
-    
-    name NVARCHAR(255),
-    ingredients TEXT,
-    instructions TEXT,
-    
+    id INT IDENTITY(1,1) PRIMARY KEY,
+    name NVARCHAR(255) NOT NULL,
+    ingredients TEXT NOT NULL,
+    instructions TEXT NOT NULL,
     calories FLOAT,
     fat FLOAT,
     carbohydrates FLOAT,
